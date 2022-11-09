1e7858ea1f270f73e50da33f287ccb26d25bffc0
NotReis <afonsoreis05@hotmail.com>
2022-11-09 12:21:24 +0000

    adicionei um comentário sobre o jorge ser fuinha e patakero

--- pap/Pages/inicio.php
+++ pap/Pages/inicio.php
@@ -28,9 +28,7 @@
     <div class="content">
         <div class="textbox">
             <h2>Make it! <br> <span> Drink </span>it!</h2>
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            <p>"o JORGE VENEZUELA É FUINHA."
             </p>
             <a href="#">Fazer uma receita</a>  
         </div>
